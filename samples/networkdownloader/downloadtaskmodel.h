e6e6b962d5e7e489983568c8f189a8d6b97a3327
vilas <vilas900420@gmail.com>
2026-03-28 22:35:39 +0800

    fix(compilation): change enum class to enum for Column
    Change Column from 'enum class' to 'enum' to fix GCC compilation
    error in switch statement.
    
    Problem:
    - index.column() returns int
    - enum class Column cannot be implicitly converted to int
    - switch(index.column()) with case Column::ColumnFileName: fails
    
    Solution:
    - Remove 'class' keyword to make it a regular enum
    - Regular enums can implicitly convert to int
    - This matches Qt's typical coding style
    
    Fixes Linux GCC error:
      could not convert 'Column::ColumnFileName' from 'Column' to 'int'

--- samples/networkdownloader/downloadtaskmodel.h
+++ samples/networkdownloader/downloadtaskmodel.h
@@ -14,7 +14,7 @@ class NetworkDownloadTaskModel : public QAbstractTableModel
     Q_OBJECT
 
 public:
-    enum class Column : int
+    enum Column
     {
         ColumnFileName = 0,
         ColumnFileSize = 1,
